de180a4bcc699c1d30fea5e24a0fbfaf70b2b3e4
Nurmukhamed Zhanibek <zhanibek.n.karaganda@gmail.com>
2019-10-02 14:28:09 +0600

    feat: add a solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.answer;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.answer = Math.round((this.min + this.max) / 2);
+        return this.answer;
     }
 
     lower() {
-
+        this.max = this.answer;
     }
 
     greater() {
-
+        this.min = this.answer;
     }
 }
 
